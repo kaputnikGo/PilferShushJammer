29396760f4edb6b9f9ffb7fbc76230d6a24adce5
kapGo <kaputnikGo@gmail.com>
2018-10-23 00:06:21 +1100

    fix logic error recording state check

--- app/src/main/java/cityfreqs/com/pilfershushjammer/MainActivity.java
+++ app/src/main/java/cityfreqs/com/pilfershushjammer/MainActivity.java
@@ -19,6 +19,7 @@ import android.os.Bundle;
 import android.text.Spannable;
 import android.text.method.ScrollingMovementMethod;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -44,7 +45,7 @@ import androidx.core.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
     //private static final String TAG = "PilferShush_Jammer";
-    public static final String VERSION = "2.2.2.4";
+    public static final String VERSION = "2.2.2.5";
     // note:: API 23+ AudioRecord READ_BLOCKING const
     // note:: MediaRecorder.AudioSource.VOICE_COMMUNICATION == VoIP
 
@@ -107,7 +108,6 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         passiveJammerButton = findViewById(R.id.run_passive_button);
         passiveJammerButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                //passiveStateCheck(isChecked);
                 if (isChecked) {
                     runPassive();
                 }
@@ -461,14 +461,14 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     private void runPassive() {
         if (passiveJammer != null && !PASSIVE_RUNNING) {
             if (passiveJammer.startPassiveJammer()) {
+                PASSIVE_RUNNING = true;
                 if (!passiveJammer.runPassiveJammer()) {
-                    // check for errors in running
-                    passiveJammerButton.toggle();
+                    // has record state errors
                     stopPassive();
+                    passiveJammerButton.toggle();
                 }
                 else {
                     entryLogger(getResources().getString(R.string.main_scanner_3), true);
-                    PASSIVE_RUNNING = true;
                     notifyManager.notify(NOTIFY_PASSIVE_ID, notifyPassiveBuilder.build());
                 }
             }
